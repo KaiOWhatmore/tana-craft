3f763e60fb60761cd87bb10ba3304baef6137c45
KaiOWhatmore <oscar1995@live.com.au>
2025-01-18 17:09:07 +1100

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+
+console.log("Firebase project key :", process.env.REACT_APP_FIREBASE_PROJECT_ID);
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
