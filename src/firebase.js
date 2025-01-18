ad91a108c9941c8306b2502c465aa934cd300405
KaiOWhatmore <oscar1995@live.com.au>
2025-01-18 19:35:52 +1100

    fix config

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCkG_JDIImUL8XqItLREODGNdJKQ_O-v1g",
     authDomain: "tana-craft-9adcf.firebaseapp.com",
@@ -14,8 +10,13 @@ const firebaseConfig = {
     messagingSenderId: "62209140137",
     appId: "1:62209140137:web:42340236ea8f13623090b8",
     measurementId: "G-LES2QY1FD0"
+
 };
 
-// Initialize Firebase
+
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { storage, auth, ref, getDownloadURL };
