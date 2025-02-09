45e0f98a885d1ab564d08be5e0d15cac51eeda8b
KaiOWhatmore <oscar1995@live.com.au>
2025-02-09 15:05:21 +1100

    ffmpeg test (NOT FUNCTIONING)

--- generate-small-images.sh
+++ generate-small-images.sh
@@ -1,10 +1,13 @@
-find public/images-copy -type f -exec basename {} \; | while IFS= read -r file; do
+images_pwd="public/images-copy/"
+find $images_pwd -type f -exec basename {} \; | while IFS= read -r file; do
+    # select files which do not have a correpsonding small version
     if [[ "$file" != *-small* ]]; then
         echo "Processing file: $file"
         base="${file%.*}"    # gets everything before the last dot
         ext="${file##*.}"    # gets the extension (after the last dot)
         new_file="${base}-small.${ext}"
-        touch public/images-copy/$new_file
+        # touch public/images-copy/$new_file
+        ffmpeg -i $images_pwd$file -vf scale=20:-1 $images_pwd$new_file
     else
         echo "Skipping file: $file (already modified)"
     fi
