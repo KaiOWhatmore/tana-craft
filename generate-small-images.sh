0d9e661c682b6b6b7d21f978138efbeb026cce16
KaiOWhatmore <oscar1995@live.com.au>
2025-02-09 15:01:20 +1100

    `touch` functioning small image generator

--- generate-small-images.sh
+++ generate-small-images.sh
@@ -1,5 +1,11 @@
-find . -type f -print0 | while IFS= read -r -d '' file; do
-    echo "Processing file: $file"
-    touch "small-$file"
-    # Do whatever the fuck you need with "$file"
-done
+find public/images-copy -type f -exec basename {} \; | while IFS= read -r file; do
+    if [[ "$file" != *-small* ]]; then
+        echo "Processing file: $file"
+        base="${file%.*}"    # gets everything before the last dot
+        ext="${file##*.}"    # gets the extension (after the last dot)
+        new_file="${base}-small.${ext}"
+        touch public/images-copy/$new_file
+    else
+        echo "Skipping file: $file (already modified)"
+    fi
+    done
